be86f072e454f51f5927dd1cca3ce5130a60b211
Volodymyr Bendyk <92178622+wolodwolod@users.noreply.github.com>
2022-05-13 15:17:19 +0300

    Update updateStatusContact.js

--- controllers/updateStatusContact.js
+++ controllers/updateStatusContact.js
@@ -4,17 +4,23 @@ const Contact = require("../models/contact");
 const updateStatusContact = async (req, res, next) => {
   try {     
     const { contactId } = req.params;        
-    const { favorite } = req.body;    
-        
-    const contact = await Contact.find({ _id:contactId });
-        if (!contact) {
+      const { favorite } = req.body;
+    
+      const update = await Contact.updateOne({ _id: contactId }, { favorite: favorite });
+      if (!update) {
             throw createError(404);
-    };    
-    contact[0].favorite = favorite;
+    };  
+        
+    // const contact = await Contact.find({ _id:contactId });
+    //     if (!contact) {
+    //         throw createError(404);
+    // };    
+    // contact[0].favorite = favorite;
     
-    await Contact.create(contact);
-    const result = await Contact.find({ _id: contactId });
-    
+    // await Contact.create(contact);
+    // const result = await Contact.find({ _id: contactId });
+      const result = await Contact.find({ _id: contactId });
+      
         res.json(result);
     } catch (error) {
         next(error);
