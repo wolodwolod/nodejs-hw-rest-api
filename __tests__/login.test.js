11ca5765c000d2dc011456fdce492db690bca5fe
Volodymyr Bendyk <92178622+wolodwolod@users.noreply.github.com>
2022-06-01 19:36:29 +0300

    fix test

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -6,7 +6,9 @@ require("dotenv").config();
 const app = require("../app");
 const {User} = require('../models_schemas/User');
 
-const {DB_TEST_HOST} = process.env;
+const { DB_TEST_HOST } = process.env;
+
+// const { describe, beforeAll, afterAll, beforeEach, afterEach, test, expect } = require("jest");
 
 
 describe("test auth routes", () => {
