b42960e1794f0cb8d31735ea3b8dadc30f0657b9
Volodymyr Bendyk <92178622+wolodwolod@users.noreply.github.com>
2022-06-01 21:29:30 +0300

    with no vercel

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const request = require("supertest");
 require("dotenv").config();
+require("@types/jest");
+
 
 const app = require("../app");
 const {User} = require('../models_schemas/User');
